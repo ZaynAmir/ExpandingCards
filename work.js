3de9200d4455cb0c1643d8871b5da7ab6f5199be
Zain Amir <zaynsheikhofficial@gmail.com>
2021-07-30 01:01:21 +0500

    Cards full working Initial commit

---
+++ work.js
@@ -0,0 +1,18 @@
+
+// selecting all the pannel class from the doc
+const panels = document.querySelectorAll(".pannel");
+
+panels.forEach(panel => {
+
+    panel.addEventListener("click", () => {
+        removeActiveClass();
+        panel.classList.add("active");
+    })
+})
+
+
+function removeActiveClass(){
+    panels.forEach(panel => {
+        panel.classList.remove("active");
+    })
+}
